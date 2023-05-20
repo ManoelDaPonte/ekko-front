5cb2b1bf9a106b7ccd0cd5f4fb6be1e7c27d3765
Manoel Da Ponte <131278442+ManoelDaPonte@users.noreply.github.com>
2023-05-20 20:12:04 +0200

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,21 @@
 import Image from 'next/image'
+import Head from 'next/head';
 
 export default function Home() {
   return (
-    <div>
-      <h1>
-        Quentin tu pues
-      </h1>
-    </div>
+    <>
+
+      <body>
+        <h1>Speech-To-Text App</h1>
+          <h2>Quentin tu pues</h2>
+
+        <form>
+            <input type="file" id="audioFile" accept="audio/*"></input>
+        </form>
+
+      </body>
+
+
+    </>
   )
 }
