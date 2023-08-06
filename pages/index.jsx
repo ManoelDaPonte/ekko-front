3dd89889c2351b9b39bdfd0950008fb199d827f4
Manoel Da Ponte <131278442+ManoelDaPonte@users.noreply.github.com>
2023-08-06 21:27:45 +0200

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,7 +1,7 @@
 import AudioSelectAndProcess from "../components/audio/AudioSelectAndProcess";
 import Logo from "../components/header/Logo";
+import styles from "../styles/app.module.css";
 import Button from "../components/Button";
-import styles from "../styles/App.module.css";
 
 const App = () => {
   return (
