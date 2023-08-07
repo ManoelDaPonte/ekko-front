ab6319bd9bf2c86370eef0d2c964c890850ed40f
Manoel Da Ponte <131278442+ManoelDaPonte@users.noreply.github.com>
2023-08-07 21:09:58 +0200

    Update _app.jsx

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,7 +1,7 @@
 import "../styles/Globals.css";
 
 function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />;
-  }
-  
-  export default MyApp;
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
