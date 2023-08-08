dade8bd503931c59797a71c82a78c28c46ad03a4
Manoel Da Ponte <131278442+ManoelDaPonte@users.noreply.github.com>
2023-08-08 12:07:25 +0200

    logo update

--- components/header/Logo.jsx
+++ components/header/Logo.jsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import logo from "../../public/images/vassler_The_EKKO_logo_combines_sleek_modernity_with_a_nod_to_th_09f686ca-fef7-47b5-be96-e3784af10edd.png";
+import logo from "../../public/images/logo-removebg-preview.png";
 import styles from "../../styles/header/Logo.module.css";
 
 const EkkoLogo = () => (
