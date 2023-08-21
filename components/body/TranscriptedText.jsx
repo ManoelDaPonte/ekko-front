7c8272f5bda7f3479810141397803e551204abb2
Manoel Da Ponte <131278442+ManoelDaPonte@users.noreply.github.com>
2023-08-21 17:57:09 +0200

    copy

--- components/body/TranscriptedText.jsx
+++ components/body/TranscriptedText.jsx
@@ -1,12 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "../../styles/body/TranscriptedText.module.css";
 import { LoremIpsum } from "react-lorem-ipsum";
 import { FaCopy, FaFileCode, FaFileAlt, FaClock } from "react-icons/fa";
 
 const TranscriptionDisplay = ({ transcription }) => {
-  const copyToClipboard = () => {
-    // Logic to copy the transcription text to the clipboard
-    // You can use document.execCommand('copy') or other methods here
+  const [isCopied, setIsCopied] = useState(false);
+
+  const copyToClipboard = async () => {
+    // Copy the transcription to clipboard
+    await navigator.clipboard.writeText(transcription);
+
+    // Indicate the text was copied
+    setIsCopied(true);
+
+    // After a duration, revert the copied indication
+    setTimeout(() => setIsCopied(false), 2000);
   };
 
   return (
@@ -17,14 +25,17 @@ const TranscriptionDisplay = ({ transcription }) => {
           <FaFileCode className={styles.icon} title="JSON" />
           <FaFileAlt className={styles.icon} title="TXT" />
           <FaClock className={styles.icon} title="Timecode" />
-          <FaCopy onClick={copyToClipboard} className={styles.icon} />
+          <FaCopy
+            onClick={copyToClipboard}
+            className={`${styles.icon} ${isCopied ? styles.copied : ""}`}
+          />
         </div>
       </div>
       <div className={styles.contentWrapper}></div>
       <div className={styles.contentWrapper2}></div>
       <div className={styles.scrollableBox}>
         <div className={styles.text}>{transcription}</div>
-        <LoremIpsum p={5} />
+        <LoremIpsum p={3} />
       </div>
     </div>
   );
