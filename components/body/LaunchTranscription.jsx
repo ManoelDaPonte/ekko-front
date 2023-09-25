e18dea0fc493be743ece2e11bf45f26b6199ad4f
Manoel Da Ponte <131278442+ManoelDaPonte@users.noreply.github.com>
2023-09-25 21:23:34 +0200

    audio handled

--- components/body/LaunchTranscription.jsx
+++ components/body/LaunchTranscription.jsx
@@ -9,29 +9,91 @@ const LaunchTranscription = ({
 }) => {
   const [uploading, setUploading] = useState(false);
 
-  const handleTransferFile = async (audioFile) => {
-    try {
-      setUploading(true); // Start the upload
-      const formData = new FormData();
-      formData.append("file", audioFile);
-      formData.append("language", selectedCountry || "");
-      console.log(selectedCountry);
+  const determineFunctionUrl = (file) => {
+    // Check file type (based on MIME type for simplicity)
+    const isVideo = file.type.startsWith("video/");
+    const isAudio = file.type.startsWith("audio/");
 
-      const response = await fetch(
-        "https://first-function-app-mano.azurewebsites.net/api/AudioTranscriptionAndSave?code=Qm6gpIlqsZ8xfqOyIQriQbOIEK8iSKWjriYCp5jBekCLAzFuL7OTdg==",
-        {
-          method: "POST",
-          body: formData,
-        }
-      );
+    // Check file size
+    const isLarge = file.size > 23 * 1024 * 1024; // 23 MB in bytes
 
-      if (response.ok) {
-        const responseData = await response.text();
-        console.log("Response from Azure Function:", responseData);
-        setTranscription(responseData);
-      } else {
-        console.error("Error sending the audio to Azure Function.");
+    if (isVideo)
+      return "https://first-function-app-mano.azurewebsites.net/api/UploadVideo?code=OA1fpGFHMLEOovpL4sFZGKe0bCgufobl8GY2z6N96om9AzFu__FHuw==";
+    if (isAudio && isLarge)
+      return "https://first-function-app-mano.azurewebsites.net/api/UploadLargeFile?code=usY-fUZF-nRIw5SJk2HwMD4K1u5ukAyD_3kFPkUTz675AzFuuKHPuA==";
+    if (isAudio && !isLarge)
+      return "https://first-function-app-mano.azurewebsites.net/api/UploadSmallFile?code=HsHSFrnMLCXBKZzSpv1v_65EA4swobp6yyBvHQ36c4kTAzFuV3t8dg==";
+
+    return null; // if file type doesn't match any category
+  };
+
+  const callAzureFunction = async (url, formData) => {
+    const response = await fetch(url, {
+      method: "POST",
+      body: formData,
+    });
+
+    if (!response.ok)
+      throw new Error("Error sending the file to Azure Function.");
+
+    const responseData = await response.json();
+    return responseData.blob_name; // This will return an array of blob names or UUIDs.
+  };
+
+  const getTranslation = async (blobName, language) => {
+    const payload = {
+      blob_name: Array.isArray(blobName) ? blobName : [blobName], // Check if blobName is already an array
+      language: language,
+    };
+
+    // Log the payload to the console
+    console.log("Sending payload:", JSON.stringify(payload, null, 2));
+
+    const response = await fetch(
+      `https://first-function-app-mano.azurewebsites.net/api/UploadTranscription?code=bSsy9GF_Y1_Si7Mhb74jmcAtY5fVsshxoYg98qagcjq_AzFuGsw2OA==`,
+      {
+        method: "POST",
+        body: JSON.stringify(payload),
+        headers: { "Content-Type": "application/json" },
       }
+    );
+
+    if (!response.ok) throw new Error("Error fetching translation UUID.");
+
+    const responseData = await response.json();
+    return responseData.blob_name;
+  };
+
+  const fetchTranslationResult = async (uuid) => {
+    const response = await fetch(
+      `https://first-function-app-mano.azurewebsites.net/api/DownloadTrancription?code=ppPVPaNIh8s0zO_ImAjhaKQU3qPeeYLU-o6Je0plXYGHAzFuRtqrag==&blob_name=${uuid}`
+    );
+    if (!response.ok) throw new Error("Error fetching the translation.");
+
+    const responseData = await response.json();
+    return responseData.result;
+  };
+
+  const handleTransferFile = async (audioFile) => {
+    console.log("handleTransferFile invoked");
+    try {
+      setUploading(true);
+
+      const formDataAudio = new FormData();
+      formDataAudio.append("file", audioFile);
+
+      const functionUrl = determineFunctionUrl(audioFile);
+      if (!functionUrl) {
+        alert("Unsupported file type!");
+        return;
+      }
+
+      const UUID = await callAzureFunction(functionUrl, formDataAudio);
+      const translationUUID = await getTranslation(UUID, selectedCountry);
+      const translation = await fetchTranslationResult(translationUUID);
+
+      console.log("Translation:", translation);
+      setTranscription(translation);
     } catch (error) {
       console.error("Error:", error);
     } finally {
